41196278ff4aed91f1fa77a3594f92648094b327
SonarJayesh <jayeshsonar2824@gmail.com>
2024-12-15 16:40:58 +0530

    [ADDED]: JAYESH Php Code for Perform Operations of two no using Arithmatic Operator

---
+++ ArithmaticOperator.php
@@ -0,0 +1,17 @@
+<?php
+	$a=10;
+	$b=20;
+
+	$add=$a+$b;
+	$sub=$a-$b;
+	$mul=$a*$b;
+	$div=$a/$b;
+	$mod=$a%$b;
+	
+	echo("Addition= $add <br>");
+	echo("Subtraction= $sub <br>");
+	echo("Multiplication= $mul <br>");
+	echo("Division= $div <br>");
+	echo("Modulus= $mod <br>");
+
+?>
