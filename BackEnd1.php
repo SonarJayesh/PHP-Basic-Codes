ecbe8da13356e2fe2ec41de2012cfd35f298532a
SonarJayesh <jayeshsonar2824@gmail.com>
2024-12-17 21:31:48 +0530

    [ADDED]: JAYESH Php code for Simple input the Roll,Name,City in Front End and print the backEnd side

---
+++ BackEnd1.php
@@ -0,0 +1,9 @@
+<?php
+    $r=$_POST['roll'];
+    $n=$_POST['name'];
+    $c=$_POST['city'];
+
+    echo"<h1> Roll = $r </h1>";
+    echo"<h1> Name = $n </h1>";
+    echo"<h1> City = $c </h1>";
+?>
