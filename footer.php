fc6a4343bb0d0a9839de45b9c1169a535ece19d7
SonarJayesh <jayeshsonar2824@gmail.com>
2024-12-29 18:52:59 +0530

    [ADDED]: JAYESH Php Code for Footer Page in php

---
+++ footer.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Web Page</title>
+</head>
+<body>
+    <h1 align="center" style="background-color:green; height: 50px;"> Footer Section</h1>
+</body>
+</html>
