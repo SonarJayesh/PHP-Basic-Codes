eb8d849e5cf010f24e03f450804bda7549dbc476
SonarJayesh <jayeshsonar2824@gmail.com>
2024-12-30 21:04:11 +0530

    [ADDED]: JAYESH Php Code for Header Section in Php Web Page

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Eeb Page</title>
+</head>
+<body>
+    <h1 align="center"
+    style="background-color: pink; height: 100px;"> Header Section</h1>
+</body>
+</html>
